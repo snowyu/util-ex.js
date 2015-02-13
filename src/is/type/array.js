fb3228c1690ae64854c968b666bc1e7c81927dc2
Riceball LEE <snowyu.lee@gmail.com>
2015-02-13 08:56:50 +0800

    * enhance the compatibility of isArray

--- src/is/type/array.js
+++ src/is/type/array.js
@@ -1,1 +1,5 @@
-module.exports = Array.isArray
+var toString = Object.prototype.toString;
+
+module.exports = Array.isArray ? Array.isArray : function isArray(obj) {
+    return toString.call(obj) === "[object Array]";
+  };
