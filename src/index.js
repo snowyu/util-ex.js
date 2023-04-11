61a93bc5b631e3d3ef0aeb74b722a5ba349af654
Riceball LEE <snowyu.lee@gmail.com>
2023-04-11 19:24:13 +0800

    fix(re-export): defineProperty

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export * from './_extend.js';
 export * from './clone-object.js';
 export * from './clone-properties-to.js';
 export * from './createFunction.js';
+export * from './defineProperty.js';
 export * from './extend.js';
 export * from './format.js';
 export * from './inject.js';
--- src/is/index.js
+++ src/is/index.js
@@ -1,7 +1,7 @@
-export * from './empty.js';
 // eslint-disable-next-line import/export
 export * from './empty-function.js';
 export * from './empty-object.js';
+export * from './empty.js';
 export * from './in.js';
-export * from './type/index.js';
 export * from './string/index.js';
+export * from './type/index.js';
