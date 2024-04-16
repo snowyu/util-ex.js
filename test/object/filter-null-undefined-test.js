54a3bc953dce5e8647fe74033c8838768f30db99
Riceball LEE <snowyu.lee@gmail.com>
2024-04-16 20:32:06 +0800

    test: remove debug

--- test/object/filter-null-undefined-test.js
+++ test/object/filter-null-undefined-test.js
@@ -4,7 +4,7 @@ import {expect} from "chai";
 import {filterNullUndefined} from '../../src/index.js';
 
 
-describe.only('filterNullUndefined', () => {
+describe('filterNullUndefined', () => {
   it('should filter null and undefined values for object', () => {
     let result = filterNullUndefined({a: 1, b: 2, c: null, d: undefined,e: false, e: 0, f: ''})
     expect(result).to.deep.equal({a: 1, b: 2, e: false, e: 0, f: ''});
